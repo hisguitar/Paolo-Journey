1038228aad7fd163b3ab0395be0f8784ae14cc8d
Guitar <wachsirin.sara@gmail.com>
2024-09-02 03:17:29 +0700

    Auto save & load excel file

--- Paolo-Journey/Assets/Scripts/CSVWriter.cs
+++ Paolo-Journey/Assets/Scripts/CSVWriter.cs
@@ -1,139 +1,208 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.IO;
-using static CSVWriter;
+using System.Collections.Generic;
 
 /// <summary>
-/// For now!
-/// Once you've exported your data,
-/// you should move 'Statistical Datas.csv' file to another location before starting to create the next set of data.
+/// Manages CSV file operations including creating, updating, and writing statistical data.
 /// </summary>
 public class CSVWriter : SingletonPersistent<CSVWriter>
 {
-    #region CSVWriter
-    private string filename = "";
+	private string filename = "";
 
-    [System.Serializable]
-    public class StatisticalData
+	[System.Serializable]
+	public class StatisticalData
+	{
+		[Tooltip("Day/Month/Year")] public string date;
+		public int interestNebulizers = 0;
+		public int interestDressingWounds = 0;
+		public int interestWashingHands = 0;
+		public int interestTestChildren = 0;
+		public int interestIQTest = 0;
+	}
+
+	[System.Serializable]
+	public class DataList
+	{
+		public List<StatisticalData> statisticalDatas = new List<StatisticalData>();
+		private string lastDate = "";
+
+		public void CreateNewStatisticalData()
+		{
+			StatisticalData newStatisticalData = new StatisticalData();
+			string currentDate = System.DateTime.Now.ToString("dd/MM/yyyy");
+			
+			if (currentDate != lastDate)
+			{
+				lastDate = currentDate;
+			}
+			newStatisticalData.date = currentDate;
+			
+			statisticalDatas.Add(newStatisticalData);
+		}
+
+		public void WriteCSV(string path)
+		{
+			using (TextWriter textWriter = new StreamWriter(path, false))
+			{
+				textWriter.WriteLine(
+					"DD/MM/YYYY, " +
+					"Interest Nebulizers, " +
+					"Interest Dressing Wounds, " +
+					"Interest Washing Hands, " +
+					"Interest Test Children, " +
+					"Interest IQ-Test");
+
+				foreach (var data in statisticalDatas)
+				{
+					textWriter.WriteLine(
+						data.date + "," +
+						data.interestNebulizers + "," +
+						data.interestDressingWounds + "," +
+						data.interestWashingHands + "," +
+						data.interestTestChildren + "," +
+						data.interestIQTest);
+				}
+			}
+		}
+	}
+
+	public DataList dailyDataList = new ();
+
+	private void Start()
+	{
+		filename = Application.dataPath + "/Statistical Datas.csv";
+		InitializeData();
+	}
+	
+	private void OnGUI()
     {
-        [Tooltip("Day/Month/Year")] public string date;
-        public int interestNebulizers = 0;
-        public int interestDressingWounds = 0;
-        public int interestWashingHands = 0;
-        public int interestTestChildren = 0;
-        public int interestIQTest = 0;
-    }
-
-    [System.Serializable]
-    public class DataList
-    {
-        public StatisticalData[] statisticalDatas;
-        private string lastDate = "";
-
-        // Enter date to date parameter & Create new statistical data list
-        public void CreateNewStatisticalData()
+        if (SceneManager.GetActiveScene().name == "PaoloJourney")
         {
-            StatisticalData newStatisticalData = new StatisticalData();
+            GUIStyle buttonStyle = GUI.skin.button;
+            buttonStyle.fontSize = 18;
 
-            #region Enter date to date parameter
-            string currentDate = System.DateTime.Now.ToString("dd/MM/yyyy");
-            if (currentDate != lastDate)
+            // Create button for 'Export to Excel'
+            if (GUI.Button(new Rect(10, 10, 140, 30), "Export to Excel", buttonStyle))
             {
-                lastDate = currentDate;
+                dailyDataList.WriteCSV(filename); // Write CSV with current data
             }
-            newStatisticalData.date = currentDate;
-            #endregion
 
-            #region Create new statistical data
-            if (statisticalDatas == null)
+            // Create button for 'New Date Statistical Data'
+            if (GUI.Button(new Rect(160, 10, 240, 30), "Create New Statistical Data", buttonStyle))
             {
-                statisticalDatas = new StatisticalData[1];
+                CreateNewStatisticalData();
+                dailyDataList.WriteCSV(filename); // Write new data to CSV
             }
-            else
-            {
-                StatisticalData[] newStatisticalDatas = new StatisticalData[statisticalDatas.Length + 1];
-                for (int i = 0; i < statisticalDatas.Length; i++)
-                {
-                    newStatisticalDatas[i] = statisticalDatas[i];
-                }
-                statisticalDatas = newStatisticalDatas;
-            }
-            statisticalDatas[statisticalDatas.Length - 1] = newStatisticalData;
-            #endregion
         }
     }
+	
+	private void InitializeData()
+	{
+		if (File.Exists(filename))
+		{
+			// Check last recorded date in the existing file
+			string lastDate = GetLastDateFromCSV();
+			string currentDate = System.DateTime.Now.ToString("dd/MM/yyyy");
 
-    public DataList dailyDataList = new();
+			if (currentDate != lastDate)
+			{
+				dailyDataList.CreateNewStatisticalData();
+				dailyDataList.WriteCSV(filename); // Write the new data to CSV file
+			}
+			else
+			{
+				LoadCSV(filename);
+			}
+		}
+		else
+		{
+			// File does not exist, create new data
+			dailyDataList.CreateNewStatisticalData();
+			dailyDataList.WriteCSV(filename);
+		}
+	}
 
-    private void Start()
-    {
-        filename = Application.dataPath + "/Statistical Datas.csv";
+	private string GetLastDateFromCSV()
+	{
+		string lastDate = "";
+		using (var reader = new StreamReader(filename))
+		{
+			// Skip header
+			reader.ReadLine();
 
-        CreateNewStatisticalData();
-    }
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				// Read last line date
+				var columns = line.Split(',');
+				lastDate = columns[0].Trim();
+			}
+		}
+		return lastDate;
+	}
 
-    // Use this function to export 'excel report file'
-    public void WriteCSV()
-    {
-        Debug.Log("WriteCSV");
-        if(dailyDataList.statisticalDatas.Length > 0)
-        {
-            TextWriter textWriter = new StreamWriter(filename, false);
-            textWriter.WriteLine(
-                "DD/MM/YYYY, " +
-                "Interest Nebulizers, " +
-                "Interest Dressing Wounds, " +
-                "Interest Washing Hands, " +
-                "Interest Test Children, " +
-                "Interest IQ-Test");
-            textWriter.Close();
+	private void LoadCSV(string path)
+	{
+		dailyDataList.statisticalDatas.Clear();
+		
+		using (var reader = new StreamReader(path))
+		{
+			// Skip header
+			reader.ReadLine();
 
-            textWriter = new StreamWriter(filename, true);
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				var columns = line.Split(',');
+				StatisticalData data = new StatisticalData
+				{
+					date = columns[0].Trim(),
+					interestNebulizers = int.Parse(columns[1].Trim()),
+					interestDressingWounds = int.Parse(columns[2].Trim()),
+					interestWashingHands = int.Parse(columns[3].Trim()),
+					interestTestChildren = int.Parse(columns[4].Trim()),
+					interestIQTest = int.Parse(columns[5].Trim())
+				};
 
-            for(int i = 0; i < dailyDataList.statisticalDatas.Length; i++)
-            {
-                textWriter.WriteLine(
-                    dailyDataList.statisticalDatas[i].date + "," +
-                    dailyDataList.statisticalDatas[i].interestNebulizers + "," +
-                    dailyDataList.statisticalDatas[i].interestDressingWounds + "," +
-                    dailyDataList.statisticalDatas[i].interestWashingHands + "," +
-                    dailyDataList.statisticalDatas[i].interestTestChildren + "," +
-                    dailyDataList.statisticalDatas[i].interestIQTest);
-            }
-            textWriter.Close();
-        }
-    }
-    #endregion
+				dailyDataList.statisticalDatas.Add(data);
+			}
+		}
+	}
 
-    #region Increment Data
-    public void CreateNewStatisticalData()
-    {
-        dailyDataList.CreateNewStatisticalData();
-    }
+	#region Increment Data
+	public void CreateNewStatisticalData()
+	{
+		dailyDataList.CreateNewStatisticalData();
+		dailyDataList.WriteCSV(filename); // Write new data to file after creation
+	}
 
-    public void IncrementInterestNebulizers(StatisticalData data)
-    {
-        
-        data.interestNebulizers += 1;
-    }
+	public void IncrementInterest(string fieldName, StatisticalData data)
+	{
+		switch (fieldName)
+		{
+			case "Nebulizers":
+				data.interestNebulizers += 1;
+				break;
+			case "DressingWounds":
+				data.interestDressingWounds += 1;
+				break;
+			case "WashingHands":
+				data.interestWashingHands += 1;
+				break;
+			case "TestChildren":
+				data.interestTestChildren += 1;
+				break;
+			case "IQTest":
+				data.interestIQTest += 1;
+				break;
+			default:
+				Debug.LogError("Invalid field name: " + fieldName);
+				return;
+		}
 
-    public void IncrementInterestDressingWounds(StatisticalData data)
-    {
-        data.interestDressingWounds += 1;
-    }
-
-    public void IncrementInterestWashingHands(StatisticalData data)
-    {
-        data.interestWashingHands += 1;
-    }
-
-    public void IncrementInterestTestChildren(StatisticalData data)
-    {
-        data.interestTestChildren += 1;
-    }
-
-    public void IncrementInterestIQTest(StatisticalData data)
-    {
-        data.interestIQTest += 1;
-    }
-    #endregion
+		// Update CSV immediately
+		dailyDataList.WriteCSV(filename);
+	}
+	#endregion
 }
--- Paolo-Journey/Assets/Scripts/SceneChanger.cs
+++ Paolo-Journey/Assets/Scripts/SceneChanger.cs
@@ -3,79 +3,81 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void ChangeScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-    }
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+	}
 
-    public void ButtonPaoloJourney()
-    {
-        SceneManager.LoadScene("PaoloJourney");
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-    }
+	public void To_PaoloJourney()
+	{
+		SceneManager.LoadScene("PaoloJourney");
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+	}
 
-    public void ButtonGameplayManager()
-    {
-        SceneManager.LoadScene("GameplayManager");
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-    }
+	public void To_GameplayManager()
+	{
+		SceneManager.LoadScene("GameplayManager");
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+	}
+	
+	#region Media
+	public void To_Media1()
+	{
+		CSVWriter.Instance.IncrementInterest("Nebulizers", CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Media1");
+	}
 
-    public void ButtonMedia1()
-    {
-        CSVWriter.Instance.IncrementInterestNebulizers(CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
+	public void To_Media2()
+	{
+		CSVWriter.Instance.IncrementInterest("DressingWounds", CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
 
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Media1");
-    }
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Media2");
+	}
 
-    public void ButtonMedia2()
-    {
-        CSVWriter.Instance.IncrementInterestDressingWounds(CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
+	public void To_Media3()
+	{
+		CSVWriter.Instance.IncrementInterest("WashingHands", CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
 
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Media2");
-    }
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Media3");
+	}
+	
+	public void To_ChooseTheCorrectWord()
+	{
+		CSVWriter.Instance.IncrementInterest("TestChildren", CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
 
-    public void ButtonMedia3()
-    {
-        CSVWriter.Instance.IncrementInterestDressingWounds(CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("ChooseTheCorrectWord");
+	}
 
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Media3");
-    }
+	public void To_IQTestMenu()
+	{
+		CSVWriter.Instance.IncrementInterest("IQTest", CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
 
-    public void ButtonGame1()
-    {
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Game1");
-    }
+		SceneManager.LoadScene("IQTestMenu");
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+	}
+	#endregion
+	#region Game
+	public void To_Game1()
+	{
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Game1");
+	}
+	
+	public void To_Game2()
+	{
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Game2");
+	}
 
-    public void ButtonGame2()
-    {
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Game2");
-    }
-
-    public void ButtonGame3()
-    {
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("Game3");
-    }
-
-    public void ButtonChooseTheCorrectWord()
-    {
-        CSVWriter.Instance.IncrementInterestTestChildren(CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
-
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-        SceneManager.LoadScene("ChooseTheCorrectWord");
-    }
-
-    public void ButtonIQTestMenu()
-    {
-        CSVWriter.Instance.IncrementInterestIQTest(CSVWriter.Instance.dailyDataList.statisticalDatas[^1]);
-
-        SceneManager.LoadScene("IQTestMenu");
-        SoundManager.Instance.Play(SoundManager.SoundName.Click);
-    }
+	public void ButtonGame3()
+	{
+		SoundManager.Instance.Play(SoundManager.SoundName.Click);
+		SceneManager.LoadScene("Game3");
+	}
+	#endregion
 }
