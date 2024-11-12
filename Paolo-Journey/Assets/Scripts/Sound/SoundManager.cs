6e07ccbdb192f64ff1e008433e2c56a624ffa099
Guitar <wachsirin.sara@gmail.com>
2024-11-12 19:44:58 +0700

    .

--- Paolo-Journey/Assets/Scripts/Sound/SoundManager.cs
+++ Paolo-Journey/Assets/Scripts/Sound/SoundManager.cs
@@ -5,91 +5,96 @@ using UnityEngine.SceneManagement;
 
 public class SoundManager : Singleton<SoundManager>
 {
-    [SerializeField] private Sound[] sounds;
-    [SerializeField] private AudioMixerGroup sfxGroup; // เพิ่ม AudioMixerGroup สำหรับ SFX
-    private static SoundManager instance;
-    public AudioClip defaultMusic; // เพลงสำหรับ Scene ปกติ
-    public AudioClip iqTestMusic;  // เพลงสำหรับ Scene IQ Test
-    private AudioSource audioSource;
+	[SerializeField] private Sound[] sounds;
+	[SerializeField] private AudioMixerGroup sfxGroup; // เพิ่ม AudioMixerGroup สำหรับ SFX
+	private static SoundManager instance;
+	public AudioClip defaultMusic; // เพลงสำหรับ Scene ปกติ
+	public AudioClip iqTestMusic;  // เพลงสำหรับ Scene IQ Test
+	private AudioSource audioSource;
 
-    private void Start()
-    {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-            
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-            audioSource = GetComponent<AudioSource>();
-            audioSource.clip = defaultMusic; // ตั้งค่าเพลงเริ่มต้น
-            audioSource.Play();
+	private void Start()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+			
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+			audioSource = GetComponent<AudioSource>();
+			audioSource.clip = defaultMusic; // ตั้งค่าเพลงเริ่มต้น
+			audioSource.Play();
 
-            // ลงทะเบียนฟังก์ชันเมื่อ Scene โหลดเสร็จ
-            SceneManager.sceneLoaded += OnSceneLoaded;
-        }
-    }
+			// ลงทะเบียนฟังก์ชันเมื่อ Scene โหลดเสร็จ
+			SceneManager.sceneLoaded += OnSceneLoaded;
+		}
+	}
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        // เช็คชื่อ Scene และเปลี่ยนเพลงตามต้องการ
-        if (scene.name == "IQTestMenu" || scene.name == "17+" || scene.name == "Result")
-        {
-            if (audioSource.clip != iqTestMusic)
-            {
-                audioSource.clip = iqTestMusic;
-                audioSource.Play();
-            }
-        }
-        else
-        {
-            if (audioSource.clip != defaultMusic)
-            {
-                audioSource.clip = defaultMusic;
-                audioSource.Play();
-            }
-        }
-    }
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		// เช็คชื่อ Scene และเปลี่ยนเพลงตามต้องการ
+		if (scene.name == "TrueOrFalse")
+		{
+			audioSource.Stop();
+		}
+		
+		if (scene.name == "IQTestMenu" || scene.name == "17+" || scene.name == "Result")
+		{
+			if (audioSource.clip != iqTestMusic)
+			{
+				audioSource.clip = iqTestMusic;
+				audioSource.Play();
+			}
+		}
+		else
+		{
+			if (audioSource.clip != defaultMusic)
+			{
+				audioSource.clip = defaultMusic;
+				audioSource.Play();
+			}
+		}
+	}
 
-    private void OnDestroy()
-    {
-        // ยกเลิกการลงทะเบียนเมื่อ GameObject ถูกทำลาย
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-    
-    // List of sounds
-    public enum SoundName
-    {
-        ThemeSong,
-        Click,
-        Correct,
-        Wrong,
-        ClickButton1,
-        ClickButton2,
-        True,
-        False
-    }
+	private void OnDestroy()
+	{
+		// ยกเลิกการลงทะเบียนเมื่อ GameObject ถูกทำลาย
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+	
+	// List of sounds
+	public enum SoundName
+	{
+		ThemeSong,
+		Click,
+		Correct,
+		Wrong,
+		ClickButton1,
+		ClickButton2,
+		True,
+		False
+	}
 
-    // For setting sound
-    public void Play(SoundName soundName)
-    {
-        var sound = GetSound(soundName);
-        if (sound.audioSource == null)
-        {
-            sound.audioSource = gameObject.AddComponent<AudioSource>();
-            sound.audioSource.clip = sound.clip;
-            sound.audioSource.volume = sound.volume;
-            sound.audioSource.loop = sound.loop;
-            
-            sound.audioSource.outputAudioMixerGroup = sfxGroup;
-        }
-        sound.audioSource.Play();
-    }
+	// For setting sound
+	public void Play(SoundName soundName)
+	{
+		var sound = GetSound(soundName);
+		if (sound.audioSource == null)
+		{
+			sound.audioSource = gameObject.AddComponent<AudioSource>();
+			sound.audioSource.clip = sound.clip;
+			sound.audioSource.volume = sound.volume;
+			sound.audioSource.loop = sound.loop;
+			
+			sound.audioSource.outputAudioMixerGroup = sfxGroup;
+		}
+		sound.audioSource.Play();
+	}
 
-    private Sound GetSound(SoundName soundName)
-    {
-        return Array.Find(sounds, s => s.soundName == soundName);
-    }
+	private Sound GetSound(SoundName soundName)
+	{
+		return Array.Find(sounds, s => s.soundName == soundName);
+	}
 }
